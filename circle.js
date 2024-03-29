6ba3479b556da63cf878dd42d45208dea7923c9a
LanaVol <svolosh4yk@gmail.com>
2024-03-29 23:09:32 +0200

    update styles with innertext

--- circle.js
+++ circle.js
@@ -1,9 +1,11 @@
 // Variable progress, which contains the percentage of filling color
-var progress = 50;
-var circle = document.getElementById("progressCircle");
+const progress = 80;
+const circle = document.getElementById("progressCircle");
+const circleText = document.querySelector(".center-circle span");
 
-var gradientColor = "blue";
-var gradientValue = `transparent ${
+const gradientColor = "blue";
+const gradientValue = `transparent ${
   360 * (1 - progress / 100)
 }deg, ${gradientColor} 0deg`;
 circle.style.background = `conic-gradient(${gradientValue})`; /* Change gradient */
+circleText.innerHTML = `${progress}%`;
